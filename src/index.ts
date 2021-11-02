f2c4ef5457672d6f5608e6bfbb5711fc4c9c1dc9
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-02 21:31:10 +0530

    setting up fs sys in node js ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // setting up express
 import express, {Request,Response,Application} from 'express';
-
+import * as fs from 'fs';
 const app:Application = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
 // main routes
@@ -10,6 +11,11 @@ app.get("/", (req:Request, res:Response):void => {
     res.send("Hello Typescript with Node.js!")
   });
 
+
+app.get("/api", (req:Request , res: Response):void => {
+
+
+})
 // setting up ports
 // void is used here as thr is no type for the port to listen to.
   app.listen(PORT, ():void => {

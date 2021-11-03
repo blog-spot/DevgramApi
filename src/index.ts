1f3a1e38654ba08481bfeb16250797d5df5713a3
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-03 16:44:36 +0530

    api working

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,7 @@ import express, {Request,Response,Application} from 'express';
 import * as fs from 'fs';
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
-
-
-// building test buildSchema
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    Source: [Post],
-    Philosophy: String,
-    quote: String,
-  }
-`);
-
-var source = `test`
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  quote: () => {
-    return schema;
-  },
-};
+var bodyparser = require('body-parser');
 
 const app:Application = express();
 const PORT = process.env.PORT || 3000;
@@ -34,16 +14,54 @@ app.use(express.json());
 app.get("/", (req:Request, res:Response):void => {
     res.send("Hello Typescript with Node.js!")
   });
+  function defineSchema(){
+    var schema = buildSchema(`
+    type mainQuery {
+      library: [String!]!
+  
+    }
+    type Mainmutation {
+      recordBook(title: String): String
+    }
+  
+    schema{
+      query: mainQuery
+      mutation: Mainmutation
+    }
+    `);
+  
+    return schema;
+  
+  }
 
+app.use("/graphql" ,graphqlHTTP({
+  // Specifiing all the options here.
+  schema:defineSchema(),
+  rootValue: {
+    library: getLibrary()
+  },
+  graphiql: true
 
-  app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  }));
+})
+);
+
 
 // setting up ports
 // void is used here as thr is no type for the port to listen to.
   app.listen(PORT, ():void => {
     console.log(`Server Running here 👉 https://localhost:${PORT}`);
   });
+
+
+
+
+function getLibrary(){
+  return [
+
+
+    "A tale of two cicties",
+    "in the chest of a womam",
+    "things fall apart"
+
+  ];
+}

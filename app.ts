f31f963821949261c7187d29976d1ceca538b4fb
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-06 12:39:21 +0530

    definigh the schema again

--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
-import express from "express";
-const app = express();
+import express, {Request,Response,Application} from 'express';
+import * as fs from 'fs';
+const app:Application = express();
+app.use(express.json());
 
+app.get("/api", (req:Request , res: Response):void => {
+    fs.readFile('./views/test.json' , 'utf8' , (err , data ) => {
+      if(err){
+        throw err;
+      }
+      res.send(JSON.parse(data));
+    })
 
+    
 app.get('/', (req, res) => {
     res.send('Hello from express and typescript');
 });

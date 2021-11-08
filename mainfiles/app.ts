b2f58234f3b704a22045332f485c3eaee460f3ef
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-08 10:46:08 +0530

    setting up files

--- mainfiles/app.ts
+++ mainfiles/app.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 3000;
+const  bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+import * as fs from 'fs';
+
 
 app.use("/",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");

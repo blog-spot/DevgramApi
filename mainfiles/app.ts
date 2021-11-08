db38971f7f8bde6823a223c30ae313201bc5708f
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-08 10:48:08 +0530

    added new route

--- mainfiles/app.ts
+++ mainfiles/app.ts
@@ -12,4 +12,12 @@ app.use("/",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
 
+app.get('/api' , (req,res) => {
+    fs.readFile('./src/test.json' , 'utf8' , (err , data ) => {
+        if(err){
+          throw err;
+        }
+        res.send(JSON.parse(data));
+      })
+})
 app.listen(PORT,() => console.log(`hosting @${PORT}`));

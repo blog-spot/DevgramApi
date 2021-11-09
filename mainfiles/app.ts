04c03a5effbcb76239d1b5f4bce1597c7ad0df7e
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-09 11:06:21 +0530

    creeating new route

--- mainfiles/app.ts
+++ mainfiles/app.ts
@@ -33,15 +33,26 @@ app.get("/headers" , (req: Request, res: Response):void => {
     })
 })
 
+app.get('/quotes' , (req: Request, res: Response):void => {
+    fs.readFile('./views/topfive.json', 'utf-8' ,(err,data)=> {
+        if(err){
+            throw err
+        }
+        res.send(JSON.parse(data))
+    })
+})
+
 // test pull test from api server
 
 async function httpRequest(){
     try {
-      const URL = "https://devgramapi.herokuapp.com/"
+      const URL = "https://devgramapi.herokuapp.com/headers"
       const response = await axios.get(URL);
-      console.log(response);
-      const quotes = response.data.Github
-      console.log(quotes);
+    //   console.log(response);
+    console.log(response)
+      const source = response.data.id
+      console.log(source);
+
     } catch (error) {
       console.error(error);
     }
